bb22d80509b1dc79970acd9d5812721fa66c992a
AseemKulkarni <aseemkulkarni2006@gmail.com>
2026-02-21 17:45:51 +0000

    Added encapsulation for the Disk object in the header as well as implemented it correctly. I also added validation for setting radius as well as sconst correctness for distance method.

--- disk.cpp
+++ disk.cpp
@@ -1,18 +1,34 @@
 #include "disk.h"
 #include <cmath>
+#include <stdexcept>
 
 Disk::Disk(double x, double y, double r){
     this->x = x;
     this->y = y;
-    this->radius = r;
+    setRadius(r);
+}
+
+// Getters 
+double Disk::getX() {return x;}
+double Disk::getY() {return y;}
+double Disk::getRadius() {return radius;}
+
+//Setters
+void Disk::setX(double X) { x = X; }
+void Disk::setY(double Y) { y = Y; }
+void Disk::setRadius(double R) {
+    if (R <= 0.0){
+        throw std::invalid_argument("Radius must be positive");
+    }
+    radius = R;
 }
 
 void Disk::move(double dx, double dy){
-    this->x += dx;
-    this->y += dy;
+    x += dx;
+    y += dy;
 }
 
-double Disk::distance(Disk& other){
+double Disk::distance(const Disk& other)const{
 
     // HINT: COMPLETE THE CALCULATION OF THE DISTANCE BETWEEN this DISK AND THE DISK other
     double distance = std::sqrt(std::pow((x - other.x),2) + std::pow((y - other.y),2));
--- disk.h
+++ disk.h
@@ -1,10 +1,24 @@
 #ifndef DISK_H
 #define DISK_H
 class Disk {
-    public:
+    private:
     double x, y, radius;
+    public:
+    // getters for each of the private attributes
+    double getX();
+    double getY();
+    double getRadius();
+
+    // Setters
+    void setX(double X);
+    void setY(double Y);
+    void setRadius(double R);
+
+    //Constructor
     Disk(double x, double y, double r) ;
+
+    //Public methods
     void move(double dx, double dy) ;
-    double distance(Disk& d) ;
+    double distance(const Disk& d) const ;
 };
 #endif // !DISK_H
