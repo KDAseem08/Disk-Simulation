a1568fba3dabf8e56195cb5e7ba21fedde30ae7d
AseemKulkarni <aseemkulkarni2006@gmail.com>
2026-02-23 14:20:51 +0000

    Added ability to give CLI arguments to control parameters of the system like Number of Disks , maxDisplacement ... etc

--- main.cpp
+++ main.cpp
@@ -9,13 +9,30 @@
  *
  * This function initializes the program and manages execution.
  */
-int main() {
-    // number of disks
+int main(int argc,char* argv[]) {
+    // Just learnt that command line output is passed as argument count and argument vector to main
+    
+    // number of disks (default)
     int N = 20;
     double maxDisplacement  = 0.6;
     double dt = 0.5;
     double L = 20.0;
 
+
+    if (argc ==5){
+        // extra 4 args are correctly provided (0th arg is the program executable)
+        N = std::stoi(argv[1]);
+        maxDisplacement = std::stod(argv[2]);
+        dt = std::stod(argv[3]);
+        L = std::stod(argv[4]);
+
+    }
+    else if (argc != 1) 
+    {
+        std::cerr << "How to call executable: " << argv[0] << " <N> <maxDisplacement> <dt> <L>\n";
+        std::cerr << "Standard default arguments: " << argv[0] << " 50 0.6 0.5 20\n";
+        return 1; // exit
+    }
     // initialise the system
     System system(N, maxDisplacement,dt,L,1234);
 
